cc101fd1708a20cc12546e56f5be2d1322ec4252
nailer <nailer@pm.me>
2019-08-12 20:10:44 +0300

    add config and example env file

---
+++ config.py
@@ -0,0 +1,17 @@
+from os import getenv
+
+
+def get(key, default=None, converter=None):
+    value = getenv(key, default)
+
+    return converter(value) if converter else value
+
+
+TOKEN = get('TOKEN')
+USE_WEBHOOK = get('USE_WEBHOOK', False, bool)
+
+if USE_WEBHOOK:
+    WEBHOOK_HOST = get('WEBHOOK_HOST')
+    WEBHOOK_PORT = get('WEBHOOK_PORT', converter=int)
+    WEBHOOK_PATH = get('WEBHOOK_PATH')
+    WEBHOOK_URL = f'https://{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_PATH}'
